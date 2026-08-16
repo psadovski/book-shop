b297f87a1efe444ab8b84c2b577dd2b442aecbc3
psadovski <pawel.sadowski@clurgo.com>
2026-08-16 11:51:52 +0200

    T2 Running backend - boo dto fixes

--- book-store-api/src/main/java/pl/pssoftware/bookstoreapi/catalog/dto/BookDTO.java
+++ book-store-api/src/main/java/pl/pssoftware/bookstoreapi/catalog/dto/BookDTO.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import pl.pssoftware.bookstoreapi.catalog.entity.BookCategory;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Set;
 
@@ -19,6 +21,17 @@ public class BookDTO {
     private String isbn;
     private LocalDate releaseDate;
     private String releasePlace;
+    private String language;
+    private Integer pageCount;
+    private BigDecimal price;
+    private BookCategory category;
+    private String description;
+    private String coverUrl;
+    private BigDecimal rating;
+    private Integer reviewCount;
+    private Integer publishedYear;
+    private Integer stock;
+    private Boolean available;
     private PublisherDTO publisher;
     private Set<AuthorDTO> authors;
 }
