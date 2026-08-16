c7e84386c005d4011fd88cee3327b306fd3217d1
psadovski <pawel.sadowski@clurgo.com>
2026-08-16 11:09:28 +0200

    T2 Running backend - temporary security turn off

---
+++ book-store-api/src/main/java/pl/pssoftware/bookstoreapi/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package pl.pssoftware.bookstoreapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
